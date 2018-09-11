98ca1b4fe2df2666f5f23aee9ac90769a3b144f9
刘标 <liubiao@autohome.com.cn>
2018-09-11 14:54:40 +0800

    项目初始化

---
+++ src/test/java/cn/note4u/Note4uApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.note4u;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Note4uApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
